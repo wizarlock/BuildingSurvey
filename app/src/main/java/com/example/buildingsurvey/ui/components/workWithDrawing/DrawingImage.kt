9ed5ab643224e9943f26653f0392d5524c24eb6a
shama <shamarorostislav@gmail.com>
2024-02-06 18:54:38 +0300

    Add prototype of WorkWithDrawing

--- app/src/main/java/com/example/buildingsurvey/ui/components/workWithDrawing/DrawingImage.kt
+++ app/src/main/java/com/example/buildingsurvey/ui/components/workWithDrawing/DrawingImage.kt
@@ -1,16 +1,12 @@
 package com.example.buildingsurvey.ui.components.workWithDrawing
 
-import android.os.Environment
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.size.Size
 import com.example.buildingsurvey.ui.screens.workWithDrawing.WorkWithDrawingUiState
