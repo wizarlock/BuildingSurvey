a5c7f67fc9c2a7626bc2af15774f82bc0c9001d7
shama <shamarorostislav@gmail.com>
2024-02-12 01:55:44 +0300

    Update Label

--- app/src/main/java/com/example/buildingsurvey/ui/components/updateLabel/LabelImage.kt
+++ app/src/main/java/com/example/buildingsurvey/ui/components/updateLabel/LabelImage.kt
@@ -1,25 +1,19 @@
 package com.example.buildingsurvey.ui.components.updateLabel
 
 import androidx.compose.runtime.Composable
-import android.os.Environment
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
-import coil.size.Size
 
 @Composable
 fun LabelImage(
